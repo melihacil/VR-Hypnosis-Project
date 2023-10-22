5d470c78ed51205b5a1bd9dfbc3175202b539f76
melihacil <knghtmellomello@gmail.com>
2023-10-22 22:35:24 +0300

    Updating scripts

---
+++ Vide/Assets/AnimationClass.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationClass : MonoBehaviour
+{
+    [SerializeField] Animation _AnimationClipControl;
+    [SerializeField] AnimationClip _AnimationClip;
+    [SerializeField] bool _IsPlaying = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_IsPlaying)
+            _AnimationClipControl.Stop();
+    }
+
+    void AnimationTest()
+    {
+        this.runInEditMode = true;
+        _AnimationClipControl.clip = _AnimationClip;
+        _AnimationClipControl.Play();
+    }
+}
