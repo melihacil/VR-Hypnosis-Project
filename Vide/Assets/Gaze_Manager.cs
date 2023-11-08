48fb093b3fa47e37532fe486b29bf725eb8b47f7
melihacil <melihacil20@gmail.com>
2023-11-08 11:31:29 +0300

    Updating scripts

---
+++ Vide/Assets/Gaze_Manager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze_Manager : MonoBehaviour
+{
+
+    [SerializeField] private Camera m_playerCamera;
+
+    [SerializeField] private GameObject m_lastGazedUpon;
+    public GameObject LastGazedUpon { get { return m_lastGazedUpon; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGaze();
+    }
+
+
+    void CheckGaze()
+    {
+
+        if (m_lastGazedUpon)
+        {
+            //m_lastGazedUpon.SendMessage("NotRequired", SendMessageOptions.DontRequireReceiver);
+        }
+
+
+        Ray newRay = new Ray(m_playerCamera.transform.position, m_playerCamera.transform.rotation * Vector3.forward);
+
+        RaycastHit newHit;
+
+
+        if (Physics.Raycast(newRay, out newHit, Mathf.Infinity))
+        {
+            m_lastGazedUpon = newHit.transform.gameObject;
+
+            //m_lastGazedUpon.SendMessage("NotRequired", SendMessageOptions.DontRequireReceiver);
+        }
+
+
+
+    }
+}
